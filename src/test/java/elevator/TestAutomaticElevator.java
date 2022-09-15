3c0e8c873e691ca3b76974a1fa48b62e2caf8023
massat <massat@yokoportable>
2022-09-15 13:23:35 +0200

    Correction d'un commentaire

--- src/test/java/elevator/TestAutomaticElevator.java
+++ src/test/java/elevator/TestAutomaticElevator.java
@@ -21,7 +21,7 @@ public class TestAutomaticElevator {
 		}
 		e.stopSimulator();
 
-		// l'ascenseur est au 3ème étage
+		// l'ascenseur est au 3ème
 		assertEquals(3.0, e.getLevel());
 		// l'ascenseur est en erreur
 		assertEquals(ERROR, e.getState());
