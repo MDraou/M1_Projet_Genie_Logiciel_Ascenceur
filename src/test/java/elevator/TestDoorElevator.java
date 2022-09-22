a1239a0aec73651003ee8813cb4925f83b999baa
v16006166 <adrien.vidal.1@etu.univ-amu.fr>
2022-09-22 15:40:29 +0200

    Continuation

--- src/test/java/elevator/TestDoorElevator.java
+++ src/test/java/elevator/TestDoorElevator.java
@@ -2,11 +2,20 @@ package elevator;
 
 import org.junit.jupiter.api.Test;
 
+import static elevator.IElevator.State.UP;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestDoorElevator {
 
     @Test
     public void testDoorElevator(){
-        PanelSimulator panel = new PanelSimulator(1);
-//        assertTrue(panel.);
+        var e = new ElevatorSimulator(2, true);
+        assertEquals("-S0", e.getEvents());
+        e.openDoor();
+        assertEquals("-S0-O0", e.getEvents());
+        e.up();
+        e.stopNext();
+        while (e.getState() == UP) e.oneStep();
+        System.out.println(e.getEvents());
     }
 }
